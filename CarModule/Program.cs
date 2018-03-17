6215f2b9aa6d03eb88cb08207eb62acd86430208
MaksRvyanin <mrvyanin@gmail.com>
2018-03-17 13:31:36 +0300

    Create Project.

---
+++ CarModule/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
